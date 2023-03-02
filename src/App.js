330a1983ee5d944ac3ae23501fc06a785569fafc
Khai Tran <trankhai1478@gmail.com>
2023-03-02 22:11:42 +0700

    khoitaoduan

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import ProductPage from './pages/ProductPage';
+import DetailPage from './pages/DetailPage';
+import CartPage from './pages/CartPage';
+import OrderPage from './pages/OrderPage';
+import SearchPage from './pages/SearchPage';
+import AdminPage from './pages/AdminPage';
+import ResetScroll from './components/ResetScroll/ResetScroll';
+import MyOrderPage from './pages/MyOrderPage';
+import ChatPage from './pages/ChatPage';
+import PaymentPage from './pages/PaymentPage';
+import OrderSuccessPage from './pages/OrderSuccessPage'
+
+function App() {
+  return (
+    <div className="App">
+
+
+    </div>
+  );
+}
+
+export default App;
